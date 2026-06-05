379c4a6fd9b7f03f208c8c20eb277dfac0ec4cf0
Danydany140294 <dany140294@hotmail.com>
2026-06-06 00:08:03 +0200

    J21 : fixtures 3 roles, tests acces S3 OK

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -15,15 +15,33 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        // Création du premier admin
+        // Admin
         $admin = new User();
         $admin->setEmail('admin@dpservices.fr');
         $admin->setFirstname('Dany');
         $admin->setLastname('Admin');
         $admin->setRoles(['ROLE_ADMIN']);
         $admin->setPassword($this->hasher->hashPassword($admin, 'admin1234'));
-
         $manager->persist($admin);
+
+        // Propriétaire
+        $owner = new User();
+        $owner->setEmail('owner@dpservices.fr');
+        $owner->setFirstname('Jean');
+        $owner->setLastname('Propriétaire');
+        $owner->setRoles(['ROLE_OWNER']);
+        $owner->setPassword($this->hasher->hashPassword($owner, 'owner1234'));
+        $manager->persist($owner);
+
+        // Femme de ménage
+        $cleaner = new User();
+        $cleaner->setEmail('cleaner@dpservices.fr');
+        $cleaner->setFirstname('Marie');
+        $cleaner->setLastname('Ménage');
+        $cleaner->setRoles(['ROLE_CLEANER']);
+        $cleaner->setPassword($this->hasher->hashPassword($cleaner, 'cleaner1234'));
+        $manager->persist($cleaner);
+
         $manager->flush();
     }
 }
